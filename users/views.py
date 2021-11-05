ec399b9d71f6bc521d448a2739412e5ca245c0e5
knch1014 <knch1014@gmail.com>
2021-11-05 16:06:05 +0900

    signup 2nd수정

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 import bcrypt, re, json
 
-from django.views         import View
-from django.http          import JsonResponse
-from django.conf import settings
+from django.views import View
+from django.http  import JsonResponse
+from django.conf  import settings
 
 from .models              import User
 
@@ -17,19 +17,19 @@ class SignUpView(View):
             nickname     = data["nickname"]
 
             if not re.match('^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email):
-                return JsonResponse({'MESSAGE' : 'EMAIL_VALIDATION_ERROR'}, status=400)
+                return JsonResponse({'message' : 'EMAIL_VALIDATION_ERROR'}, status=400)
 
             if not re.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", password):
-                return JsonResponse({"MESSAGE" : 'PASSWORD_VALIDATION_ERROR'}, status=400)
+                return JsonResponse({'message' : 'PASSWORD_VALIDATION_ERROR'}, status=400)
 
             if not re.match("^01([0|1|6|7|8|9]?)([0-9]{3,4})([0-9]{4})$", phone_number):
-                return JsonResponse({"MESSAGE" : 'PHONE_NUMBER_ERROR'}, status=400)
+                return JsonResponse({'message' : 'PHONE_NUMBER_ERROR'}, status=400)
 
             if User.objects.filter(email=email).exists():
-                return JsonResponse({'MESSAGE' : 'DUPLICATED EMAIL'}, status=400)
+                return JsonResponse({'message' : 'DUPLICATED EMAIL'}, status=400)
 
             if User.objects.filter(nickname=nickname).exists():
-                return JsonResponse({'MESSAGE' : 'DUPLICATED NICKNAME'}, status=400)
+                return JsonResponse({'message' : 'DUPLICATED NICKNAME'}, status=400)
 
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
@@ -41,7 +41,10 @@ class SignUpView(View):
                 name         = name
                 )
 
-            return JsonResponse({'MESSAGE' : 'SUCCESS'}, status = 201)
+            return JsonResponse({'message' : 'SUCCESS'}, status = 201)
 
         except KeyError:
-            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'message' : 'NO_DATA'}, status=400)
