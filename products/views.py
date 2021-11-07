0689048be7d401d776de7007db281f161131e880
Kyuuu827 <lck0827@gmail.com>
2021-11-07 18:03:45 +0900

    modified products view

---
+++ products/views.py
@@ -0,0 +1,71 @@
+import json
+
+from django.http.response import JsonResponse
+from django.views         import View
+from django.db.models     import Q, Avg, Count
+
+from products.models      import *
+
+class CategoryView(View):
+    def get(self, request):
+        category_data = [{
+            'menu_id'   : menu.id,
+            'menu_name' : menu.name,
+            'image_url' : menu.image_url,
+            'category'  : [{
+                'category_id'   : category.id,
+                'category_name' : category.name,
+                'subcategory'   : [{
+                    'subcategory_id'   : subcategory.id,
+                    'subcategory_name' : subcategory.name,
+                } for subcategory in category.subcategory_set.all()],
+            } for category in menu.category_set.all()],
+        } for menu in Menu.objects.all()]
+
+        return JsonResponse({'category':category_data}, status = 200)
+
+
+class ProductListView(View):
+    def get(self, request):
+        type = request.GET.get("type")
+        sort = request.GET.get("sort")
+
+        q = Q()
+
+        if type:
+            q.add(Q(sub_category_id=type), q.AND)
+
+        products = ProductGroup.objects.filter(q)
+
+        if sort == 'best_ranking':
+            products = products.annotate(star_ranking=Avg('review__star_rate')).order_by('-star_ranking')[0:10]
+
+        if sort == 'ranking':
+            products = products.annotate(star_ranking=Avg('review__star_rate')).order_by('-star_ranking')
+
+        if sort == 'new':
+            products = products.order_by('created_at')
+
+        if sort == 'low_price':
+            products = products.order_by('displayed_price')
+        
+        if sort == 'high_price':
+            products = products.order_by('-displayed_price')
+
+        if sort == 'review':
+            products = products.annotate(review_count=Count('review')).order_by('-review_count')
+        
+        results = [
+            {
+            'id'               : product.id,
+            'company'          : product.company,
+            'product_name'     : list(product.product_set.all())[0].name,
+            'price'            : product.displayed_price,
+            'image_url'        : list(product.productimage_set.all())[0].image_url,
+            'discount_rate'    : product.discount_price,
+            'discounted_price' : round((float(product.displayed_price)-(float(product.displayed_price) * (float(product.discount_price)*0.01))),0),
+            'star_rate'        : product.review_set.aggregate(Avg('star_rate'))['star_rate__avg'],
+            'review'           : product.review_set.aggregate(Count('id'))['id__count']
+            }for product in products]
+
+        return JsonResponse({'products':results}, status = 200)
