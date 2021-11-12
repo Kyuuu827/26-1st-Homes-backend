a5e6168b00fd4ca280879bef44e5cec0839dcad9
Kyuuu827 <lck0827@gmail.com>
2021-11-12 10:54:28 +0900

    orders url 변경

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,5 @@ from orders.views import OrderItemView
 
 urlpatterns = [
 	path('', OrderItemView.as_view()),
-	path('/patch/<int:id>', OrderItemView.as_view()),
-	path('/delete/<int:id>', OrderItemView.as_view()), 
+	path('/<int:id>', OrderItemView.as_view()),
 ]
