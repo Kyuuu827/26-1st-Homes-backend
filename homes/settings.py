74e8719e8725500dbe591239a5fa6ab0fca67559
Kyuuu827 <lck0827@gmail.com>
2022-02-21 02:49:04 +0900

    MODIFY: settings.py redis 설정 추가

--- homes/settings.py
+++ homes/settings.py
@@ -160,3 +160,14 @@ CORS_ALLOW_HEADERS = (
 )
 
 ALGORITHM = ALGORITHM
+
+#Redis
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
